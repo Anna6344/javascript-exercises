ff31710993d19c1ba63ffd88d2c5e043b6b8fe0e
Anna <anna.glushkova@gmail.com>
2022-10-30 21:29:18 +0300

    Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+  if(typeof int1 !== 'number' || typeof int2 !== "number" || int1 < 0 || int2 < 0) {
+    return 'ERROR';
+  } else if(int1 < int2) {
+    let sum = int1;
+    let next = int1 + 1;
+    while(next <= int2){
+      sum += next;
+      next++
+    }
+   return sum;
+  } else if(int2 < int1) {
+    let sum = int2;
+    let next = int2 + 1;
+    while(next <= int1){
+      sum += next;
+      next++
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
