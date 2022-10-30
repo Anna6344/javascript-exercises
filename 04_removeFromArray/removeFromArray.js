75310f166882dcdf976de1fb05dbc735a4d6ec02
Anna <anna.glushkova@gmail.com>
2022-10-30 20:14:48 +0300

    Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...someArgs) {
+  let argLen = someArgs.length;
+  for(let i=0; i<argLen; i++){
+    let ind = arr.findIndex(foundIndex);
+    function foundIndex(item) {
+      return item === someArgs[i];
+    }
+    if(ind === -1) continue;
+    arr.splice(ind, 1);
+  } 
+  return arr;
 };
 
 // Do not edit below this line
