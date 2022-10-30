a073dbccbf63a012c2a922b543f172387b6c4856
Anna <anna.glushkova@gmail.com>
2022-10-30 22:27:53 +0300

    Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(degF) {
+  let degC = (degF - 32) * 5 / 9;
+  if(Number.isInteger(degC)){
+    return degC;
+  } else return Number(degC.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(degC) {
+  let degF = degC * (9 / 5) + 32;
+  if(Number.isInteger(degF)) {
+    return degF;
+  } else return Number(degF.toFixed(1));
 };
 
 // Do not edit below this line
