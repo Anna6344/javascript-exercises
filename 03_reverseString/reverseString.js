666dcaf49ba1f32c4cc135c0c87a1c734b549d78
Anna <anna.glushkova@gmail.com>
2022-10-30 18:08:44 +0300

    Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const arr = string.split('');
+  let revArr = [];
+  let order = 0;
+  for(let i=string.length-1; i >= 0; i--) {
+    revArr[order] = arr[i];
+    order++
+  }
+  return revArr.join('');
 };
 
 // Do not edit below this line
