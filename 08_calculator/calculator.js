7490a62679ed0d6f6dcb2597068d4a3a3327b1f4
Anna <anna.glushkova@gmail.com>
2022-11-26 12:21:14 +0300

    Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(...nums) {
+	let sum = 0;
+  for (let num of nums) {
+    sum += num;
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach((item) => {
+    sum += item;
+  });
+  return sum;	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  arr.forEach((item) => {
+    prod *= item;
+  });
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let initNum = num1;
+  for (let i = 1; i < num2; i++) {
+    num1 *= initNum;
+  };
+  return num1;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+  for (let i = 1; i < num + 1; i++) {
+    fact *= i;
+  };
+	return fact;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+  str = str.replace(/[^\w]/g, '');
+  const len = str.length;
+  const upStr = str.toUpperCase();
+  let flag = true;
+  let i = 0;
+  if (len % 2 === 0) {
+    for (i = 0; i < (len / 2); i++) {
+      if (upStr[i] !== upStr[len - 1 - i]) {
+      flag = false;
+      };
+    };
+    return flag;
+  } else if (len % 2 === 1) {
+      for (i = 0; i < (len / 2 - 1); i++) {
+        if (upStr[i] !== upStr[len - 1 - i]) {
+          flag = false;
+        };
+      };
+      return flag;
+  };
 };
 
 // Do not edit below this line
